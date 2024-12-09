54044f758761edbb9e2ae553b5730de5356ea9d2
Sangeetha <37259715+SangeethaKumari@users.noreply.github.com>
2024-12-09 13:34:22 -0800

    Create SemanticSimilarity.py
    A simple code to find the Semantic Similarity between two words say , cat and dogs are relatable than cat and cars
    
    # This is how your code will be called.
    # You can edit this code to try different testing cases.
    result = Answer.similar_words("tree", top_k=10)

---
+++ Semanticsimilarity.py
@@ -0,0 +1,23 @@
+# Python code​​​​​​‌‌​​​​​​‌‌‌​‌‌‌​‌‌​‌​​‌‌‌ below
+show_expected_result = False
+import pickle
+with open("./deps/deps/word_to_vector_trsf.pkl", "rb") as pk:
+    word_to_vector = pickle.load(pk)
+
+def cosine_similarity(vec_a, vec_b):
+    numerator = sum([vec_a[i] * vec_b[i] for i in range(len(vec_a))])
+    denominator = (sum([vec_a[i] ** 2 for i in range(len(vec_a))]) ** 0.5 * sum([vec_b[i] ** 2 for i in range(len(vec_b))]) ** 0.5)
+    return numerator / denominator
+
+show_expected_result = False
+show_hints = False
+
+
+def similar_words(word="tree", top_k=10):
+    return sorted(
+        word_to_vector.keys(), 
+        #key=lambda x: -len(x)
+        key=lambda x: -cosine_similarity(word_to_vector[x],word_to_vector[word])
+    )[:top_k]
+
+
